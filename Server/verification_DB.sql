43d8c8dedfa526373ad0135ccc73160dd3a51eb2
Kidrash <60612233+Kidrash@users.noreply.github.com>
2024-03-11 18:10:54 +0300

    Initial commit

---
+++ Server/verification_DB.sql
@@ -0,0 +1,7 @@
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id VARCHAR(50) NOT NULL,
+    student_name VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15),
+    email VARCHAR(100)
+);
